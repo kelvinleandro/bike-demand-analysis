425c56e2fb3279b095ed82d8005769f568f2fe93
kelvinleandro <kelvinleandro5@gmail.com>
2025-07-27 21:31:05 -0300

    feat(models): train and test models

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,5 +1,10 @@
 import numpy as np
-from sklearn.metrics import mean_squared_log_error, r2_score, mean_absolute_error
+from sklearn.metrics import (
+    mean_squared_log_error,
+    r2_score,
+    mean_absolute_error,
+    mean_squared_error,
+)
 
 
 def evaluate_model(y_true, y_pred, model_name="Modelo"):
@@ -9,14 +14,16 @@ def evaluate_model(y_true, y_pred, model_name="Modelo"):
     """
     y_pred = np.maximum(y_pred, 0)
 
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
     rmsle = np.sqrt(mean_squared_log_error(y_true, y_pred))
     r2 = r2_score(y_true, y_pred)
     mae = mean_absolute_error(y_true, y_pred)
 
     print(f"--- Desempenho do {model_name} ---")
+    print(f"RMSE (Root Mean Squared Error):       {rmse:.2f}")
     print(f"RMSLE (Root Mean Squared Log Error): {rmsle:.4f}")
     print(f"R² (Coeficiente de Determinação):    {r2:.4f}")
     print(f"MAE (Mean Absolute Error):           {mae:.2f} aluguéis")
     print("----------------------------------------")
 
-    return {"rmsle": rmsle, "r2": r2, "mae": mae}
+    return {"rmsle": rmsle, "rmse": rmse, "r2": r2, "mae": mae}
